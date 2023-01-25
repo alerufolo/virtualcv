5662be79774f2e44fced8de4500852eed998913b
Alejandro Rúfolo <alerufolo@gmail.com>
2023-01-25 16:33:03 -0300

    Primer commit CV

---
+++ action_page.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Alejandro Rúfolo</title>
+   
+    <!-- ícono pestaña web -->
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+
+    <!-- Tipografia google-->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet">
+    
+</head>
+
+<body>
+    <div class="secciones2">
+        <h2> Gracias por tu mensaje.</h2>
+        Te contestaré a la brevedad.
+    </div>
+    
+    <div>
+        <input type="button" a href="index.html"  id="boton" type="submit" value="Volver" >
+    </div>
+</body>
